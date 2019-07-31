e00a0f68f786c17963365eed1b6159597f191c99
Casey Heagerty <cheagerty@gmail.com>
2019-07-31 15:42:01 +0200

    React quete 3

---
+++ src/Lamp.js
@@ -0,0 +1,34 @@
+// Lamp.js
+import React, { Component } from 'react';
+
+class Lamp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            on: false
+        };
+    }
+
+    handleClick = () => {
+        this.setState({ on: !this.state.on});
+    };
+
+
+    render() {
+        const light = this.state.on ? 'on' : 'off';
+        return (
+            <div className="Lamp">
+                <button
+                    className={light}
+                    onClick={this.handleClick}
+                >
+                    {light.toUpperCase()}
+                </button>
+                <figure className={light} />
+            </div>
+        );
+    }
+}
+
+export default Lamp;
